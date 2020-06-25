bbb61e7280e23d421688ca55dae44a8deb7620ca
Vladimir Liutov <vs@lutov.net>
2020-06-25 13:31:52 +0300

    Revert "Update source"

--- src/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({id="i-filter", className, width=32, height=32, strokeWidth='6.25%', strokeLinejoin='round', strokeLinecap='round', color='currentcolor'}) => (
-  <svg className={className} id={id} viewBox="0 0 32 32" width={width} height={height} fill="none" stroke={color} strokeLinecap={strokeLinecap} strokeLinejoin={strokeLinejoin} strokeWidth={strokeWidth}>
-    <path d="M2 5 C2 5 6 3 16 3 26 3 30 5 30 5 L19 18 19 27 13 30 13 18 2 5Z" />
-  </svg>
-)
-
-export default Filter;
